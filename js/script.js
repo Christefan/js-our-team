a81ce6200587f4eb18cb33ad5322728149e8bf90
Christefan <chriscapod7@gmail.com>
2022-04-28 17:12:54 +0200

    Bonus 1 e 2

--- js/script.js
+++ js/script.js
@@ -37,27 +37,44 @@ const team = [
 for(let k in team){
     console.log(team[k]);
 }
-const gridObj = document.querySelector(".obj");
+const gridObj = document.querySelector(".team-card");
 for(let i=0; i < team.length;i++){
     const currentElement = team[i];
     console.log(currentElement.name, currentElement.role,currentElement.image);
-    const domElement = generateBox(currentElement.name, currentElement.role,currentElement.image)
-
-
+    const domElement1 = generateBoxImg(currentElement.image);
+    const domElement2 = generateBoxName(currentElement.name);
+    const domElement3 = generateBoxRole(currentElement.role);
+    domElement2.appendChild(domElement3);
 
     // appendo questo elemnto 
-    gridObj.append(domElement);
+    gridObj.append(domElement1,domElement2);
 }
 
 
 
-function generateBox(newobj1,newobj2,newobj3) {
-    const newElement = document.createElement("div");
+function generateBoxImg(newobj3) {
+    const newElement1 = document.createElement("div");
 
-    newElement.innerHTML = `<h1>${newobj1} - ${newobj2}</h1>`    
-
-    newElement.innerHTML =`<img src="img/${newobj3}"></img>`
+    newElement1.setAttribute('class','card-image')    
+    newElement1.innerHTML =`<img src="img/${newobj3}"></img>`;
     // ritorno elemnto
-    return newElement;
+    return newElement1;
     
+}
+function generateBoxName(newobj1) {
+
+
+    const newElement2 = document.createElement("div");
+
+    newElement2.setAttribute('class','card-text') 
+    newElement2.innerHTML =`<h3>${newobj1}</h3>`;
+    // ritorno elemnto
+    return newElement2;
+    
+}
+function generateBoxRole(newobj2){
+    const newElement3 = document.createElement("p");
+    newElement3.innerHTML =`${newobj2}`;
+    // ritorno elemnto
+    return newElement3;
 }
